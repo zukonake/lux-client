e56f52a2aba042764f68916032f649a52cd8e148
zukonake <zukonake@protonmail.com>
2018-07-31 19:58:40 +0200

    Added face culling for quads generation

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -1,11 +1,12 @@
+#include <glm/glm.hpp>
+//
+#include <lux/alias/set.hpp>
 #include <lux/util/log.hpp>
 //
 #include <data/database.hpp>
 #include <data/obj.hpp>
 #include <map/tile/tile_type.hpp>
 #include "map.hpp"
-
-#include <iostream> //TODO
 
 namespace map
 {
@@ -36,39 +37,105 @@ Chunk const *Map::operator[](chunk::Pos const &pos) const
 
 void Map::add_chunk(serial::ChunkData const &new_chunk)
 {
-    if(chunks.count(new_chunk.pos) > 0) return;
-    util::log("MAP", util::TRACE, "adding new chunk %zd, %zd, %zd",
-        new_chunk.pos.x,
-        new_chunk.pos.y,
-        new_chunk.pos.z);
-    chunks[new_chunk.pos] = { };
+    using namespace linear;
+    chunk::Pos const &chunk_pos = new_chunk.pos;
+    if(chunks.count(chunk_pos) > 0) return;
+
+    auto &chunk = chunks[chunk_pos];
+
+    util::log("MAP", util::TRACE, "adding new chunk %d, %d, %d",
+        chunk_pos.x,
+        chunk_pos.y,
+        chunk_pos.z);
+    chunks[chunk_pos] = { };
     //TODO optimize
-    auto &chunk = chunks[new_chunk.pos];
-    chunk.vertices.reserve(chunk::TILE_SIZE * block_model.vertices.size());
-    chunk.indices.reserve(chunk::TILE_SIZE * block_model.indices.size());
     glGenBuffers(1, &chunk.vbo_id);
     glGenBuffers(1, &chunk.ebo_id);
+
+    /* MESHING BEGINS */
+    // TODO put it into another function
+
+    auto is_solid = [&](map::Pos const &pos) -> bool
+    {
+        const Hash void_hash = std::hash<String>()("void");
+        if(chunk::to_pos(pos) != chunk_pos) return false;
+        else return new_chunk.tiles[chunk::to_index(pos)].db_hash != void_hash;
+    };
+
+    enum QuadSide
+    {
+        N_X = 0,
+        P_X = 1,
+        N_Y = 2,
+        P_Y = 3,
+        N_Z = 4,
+        P_Z = 5,
+    };
     render::Index index_offset = 0;
+    const glm::vec3 quads[6][4] =
+    {
+        {{0.0, 0.0, 0.0},
+         {0.0, 1.0, 0.0},
+         {0.0, 1.0, 1.0},
+         {0.0, 0.0, 1.0}},
+        {{1.0, 0.0, 0.0},
+         {1.0, 1.0, 0.0},
+         {1.0, 1.0, 1.0},
+         {1.0, 0.0, 1.0}},
+        {{0.0, 0.0, 0.0},
+         {1.0, 0.0, 0.0},
+         {1.0, 0.0, 1.0},
+         {0.0, 0.0, 1.0}},
+        {{0.0, 1.0, 0.0},
+         {1.0, 1.0, 0.0},
+         {1.0, 1.0, 1.0},
+         {0.0, 1.0, 1.0}},
+        {{0.0, 0.0, 0.0},
+         {1.0, 0.0, 0.0},
+         {1.0, 1.0, 0.0},
+         {0.0, 1.0, 0.0}},
+        {{0.0, 0.0, 1.0},
+         {1.0, 0.0, 1.0},
+         {1.0, 1.0, 1.0},
+         {0.0, 1.0, 1.0}}
+    };
+
     for(SizeT i = 0; i < chunk::TILE_SIZE; ++i)
     {
         chunk.tiles[i].type = db.tile_types.at(new_chunk.tiles[i].db_hash);
-        if(chunk.tiles[i].type->id != "void")
+        map::Pos map_pos = chunk::to_map_pos(chunk_pos, i);
+        if(is_solid(map_pos))
         {
-            map::Pos map_pos = chunk::to_map_pos(new_chunk.pos, i);
-            for(auto const &vertex : block_model.vertices)
+            map::Pos offsets[6] = {{-1,  0,  0},
+                                   { 1,  0,  0},
+                                   { 0, -1,  0},
+                                   { 0,  1,  0},
+                                   { 0,  0, -1},
+                                   { 0,  0,  1}};
+            for(SizeT side = 0; side < 6; ++side)
             {
-                chunk.vertices.emplace_back(vertex.pos + (glm::vec3)map_pos,
-                    vertex.col);
+                if(!is_solid(map_pos + offsets[side]))
+                {
+                    for(unsigned j = 0; j < 4; ++j)
+                    {
+                        glm::vec4 col = glm::vec4(quads[side][j], 1.0);
+                        chunk.vertices.emplace_back((glm::vec3)map_pos +
+                                                    quads[side][j], col);
+                    }
+                    for(auto const &idx : {0, 1, 2, 2, 3, 0})
+                    {
+                        chunk.indices.emplace_back(idx + index_offset);
+                    }
+                    index_offset += 4;
+                }
             }
-            for(auto const &index : block_model.indices)
-            {
-                chunk.indices.emplace_back(index + index_offset);
-            }
-            index_offset += block_model.vertices.size();
         }
     }
+    /* MESHING ENDS */
+
     chunk.vertices.shrink_to_fit();
     chunk.indices.shrink_to_fit();
+
     glBindBuffer(GL_ARRAY_BUFFER, chunk.vbo_id);
     glBufferData(GL_ARRAY_BUFFER,
                  sizeof(render::Vertex) * chunk.vertices.size(),
