aa2a91b203a34155a19df4570bc0b84d8d33642a
zukonake <zukonake@protonmail.com>
2018-08-09 08:15:48 +0200

    Fog is closer and smoother

--- glsl/vertex-2.1.glsl
+++ glsl/vertex-2.1.glsl
@@ -18,7 +18,7 @@ void main()
     gl_Position = projection * view * world * vec4(pos, 1.0);
     f_tex_pos = tex_pos * tex_size;
 #ifdef ENABLE_FOG
-    float fog = 10.0 - gl_Position.z;
+    float fog = 1.0 - (gl_Position.z / 5.0);
     f_col = col * min(1.0, max(0.0, fog));
 #else
     f_col = col;
