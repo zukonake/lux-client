2aa02cdea93f103e90ec320a08402fe8757e3ac3
zukonake <zukonake@protonmail.com>
2018-08-09 12:00:40 +0200

    Added mipmaps

--- src/render/tileset.cpp
+++ src/render/tileset.cpp
@@ -22,12 +22,29 @@ Vec2<U32> Tileset::init(String const &path)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y,
                  0, GL_RGBA, GL_UNSIGNED_BYTE, image.data());
 
+    assert((size.x & (size.x - 1)) == 0); //assert powers of 2
+    assert((size.y & (size.y - 1)) == 0); //
+    assert(size.x == size.y); //only this is supported now
+
+    Vector<U8> mipmap_buf;
+    for(U32 s = size.x / 2, i = 1; s > 0; s /= 2, ++i)
+    {
+        U32 mul = size.x / s;
+        mipmap_buf.resize(s * s * 4);
+        for(U32 p = 0; p < s * s; ++p)
+        {
+            std::memcpy(&mipmap_buf[p * 4],
+                        &image[(((p % s) * mul) +
+                                ((p / s) * size.x * mul)) * 4], 4);
+        }
+        glTexImage2D(GL_TEXTURE_2D, i, GL_RGBA, s, s,
+                     0, GL_RGBA, GL_UNSIGNED_BYTE, mipmap_buf.data());
+    }
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-    // TODO ^ no mipmaps unless I find a way to generate them reliably
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     return size;
 }
