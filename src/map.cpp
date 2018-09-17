5b75b70a1b40ce59894631ceb15920bba0e5f581
zukonake <zukonake@protonmail.com>
2018-09-17 14:20:04 +0200

    Fixed chunk coords being displayed as unsigned

--- src/map.cpp
+++ src/map.cpp
@@ -14,7 +14,7 @@ bool is_chunk_loaded(ChkPos const& pos) {
 void load_chunk(NetServerSignal::MapLoad::Chunk const& net_chunk) {
     ChkPos pos = net_order(net_chunk.pos);
     LUX_LOG("loading chunk");
-    LUX_LOG("    pos: {%zu, %zu, %zu}", pos.x, pos.y, pos.z);
+    LUX_LOG("    pos: {%zd, %zd, %zd}", pos.x, pos.y, pos.z);
     if(is_chunk_loaded(pos)) {
         LUX_LOG("chunk already loaded, ignoring it");
         return;
